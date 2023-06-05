45cf28b716c1281cace77554c3f69162769179b4
SantoshThapaa <st708611@gmail.com>
2023-06-05 11:20:46 +0545

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import First from './First';
+
+const App=()=> {
+  return (
+    <div className="App">
+      <h2>Durshikshya Transforming education</h2>
+      <hr/>
+      <h1>Welcome to Durshikshya</h1>
+      <First/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/First.js
@@ -0,0 +1,14 @@
+// rafce 
+import React from 'react'
+
+const First = () => {
+  return (
+    <>
+    <h1>This is the first component</h1>
+    <h1>We are using functional component</h1>
+    </>
+    
+  )
+}
+
+export default First
